4b25980a2583a8dbb327d30083baf0ddecac39e3
muxammed <muxammedsaparow@gmail.com>
2020-01-12 17:50:28 +0500

    start12012020

---
+++ MultiplyNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiplyNumber
+//
+//  Created by ideal on 1/12/20.
+//  Copyright © 2020 fmproduct.pager.app. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
